f83c304e0a24aabf3f64e8061c50c920ab406af3
Tiago <tiago.s.1189@gmail.com>
2018-05-13 09:05:37 +0200

    added file loader device to read data from folders

--- src/devices/FileLoader.js
+++ src/devices/FileLoader.js
@@ -1,11 +1,46 @@
 
+var p = require('path');
+
 class FileLoader{
-  constructor(props, deps = {}){
+  constructor(params, deps = {}){
+    this.deviceId = params.deviceId;
+
+    this.onDataCb = null;
+    const loadFrom = p.join( params.location , params.type );
+
+    console.log(`FileLoader registered to path ${loadFrom}`)
 
     // /\.(gif|jpg|jpeg|tiff|png)$/i
-    deps.chokidar.watch('*.js|*.txt').on('add', (path, stats ) => {
-      console.log(path);
+    deps.chokidar.watch(loadFrom,{
+      ignoreInitial:true
+    }).on('add', (path, stats ) => {
+      deps.base64.encode(path, (err, base64) => {
+        if(this.onDataCb){
+          this.onDataCb({
+            path,
+            name: p.basename(path),
+            base64,
+            createdAt: stats.atime
+          });
+        }
+      });
     });
+  }
+
+  getID(){
+    return this.deviceId;
+  }
+
+  onData(cb){
+    this.onDataCb = cb;
+  }
+
+  setRemoteDevice(remoteDevice){
+    this.remoteDevice = remoteDevice;
+  }
+
+  getRemoteDevice(){
+    return this.remoteDevice
   }
 }
 
