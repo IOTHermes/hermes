97721c59b0b0d19c998ca8268b4dfd116483f2d7
Tiago <tiago.s.1189@gmail.com>
2018-05-13 09:06:34 +0200

    added bridge to link local with remote devices

---
+++ src/bridge/index.js
@@ -0,0 +1,25 @@
+const _ = require('lodash')
+
+// Links local devices with remotes
+
+module.exports = (localDevices , remote) =>{
+  //console.log(localDevices)
+
+  return remote.getDevices()
+  .then(remoteDevices =>{
+    console.log(`Retrieved ${remoteDevices.length} devices from the remote`)
+    console.log('Matching remote with local devices')
+
+    remoteDevices.forEach(remoteDevice => {
+      //Finding match with localDevice
+      const matchIndex = _.findIndex(localDevices, ld => ld.getID()+'' == remoteDevice.getID()+'')
+
+      if(matchIndex > -1){
+        console.log(`  + Found a match for ${remoteDevice.getID()} - ${remoteDevice.getAlias()}`)
+        localDevices[matchIndex].setRemoteDevice(remoteDevice)
+      }
+    });
+    
+    return localDevices;
+  })
+}
