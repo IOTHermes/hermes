c92e96c5b44339b9d7d3a25d932548289e12216e
Tiago <tiago.s.1189@gmail.com>
2018-05-13 09:06:19 +0200

    added device object

--- src/devices/index.js
+++ src/devices/index.js
@@ -1,11 +1,13 @@
 
-
 const chokidar = require('chokidar')
+const base64 = require('file-base64');
 
-
+/* Device types */
 const FileLoader = require('./FileLoader');
 
-const fileLoader = new FileLoader({},{chokidar});
+module.exports = (type,params) => {
+  return new FileLoader(params,{chokidar,base64});
+}
 
 // file watcher https://nodejs.org/docs/latest/api/fs.html#fs_class_fs_fswatcher
 // https://www.npmjs.com/package/fs-extra
