da52294af38a71325856336b00d8e4fd6998e82f
Tiago <tiago.s.1189@gmail.com>
2018-05-13 09:06:52 +0200

    main file to manage them all

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 
+//const devices = require('./devices')
+//const remotes = require('./remotes')
+const configManager = require('./config-manager')
+const bridge = require('./bridge')
 
-// TODO add here the common data collection feed
+configManager()
+.then(res=>bridge(res.localDevices,res.remote))
+.then(devices => {
+  devices.forEach(device =>{
+    console.log(`Listening to data from ${device.getRemoteDevice().getAlias()}`)
+    device.onData( data => {
+      console.log(`Got data from ${device.getRemoteDevice().getAlias()}`,data)
+      console.log(device.getRemoteDevice().getProperties())
+
+      device.getRemoteDevice().addLog(data);
+    })
+  })
+})
