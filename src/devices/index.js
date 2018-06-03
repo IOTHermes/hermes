3c9649799328ff201892c769aa7a6a4ece24c1e0
Tiago <tiago.s.1189@gmail.com>
2018-06-04 00:43:25 +0200

    implemented proper device selector

--- src/devices/index.js
+++ src/devices/index.js
@@ -1,12 +1,18 @@
 
-const chokidar = require('chokidar')
-const base64 = require('file-base64');
+const deps = {
+  chokidar: require('chokidar'),
+  base64: require('file-base64'),
+  i2c_htu21d: require('htu21d-i2c')
+}
 
-/* Device types */
-const FileLoader = require('./FileLoader');
+/* Device models */
+const deviceModels = {
+  FileLoader: require('./FileLoader'),
+  HTU21D: require('./HTU21D')
+}
 
 module.exports = (type,params) => {
-  return new FileLoader(params,{chokidar,base64});
+  return new deviceModels[type](params,deps);
 }
 
 // file watcher https://nodejs.org/docs/latest/api/fs.html#fs_class_fs_fswatcher
